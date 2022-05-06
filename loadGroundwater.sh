869d12eb581aa3d1a211c61f5b3097cafeef4e68
Brian Ballsun-Stanton <Denubis@users.noreply.github.com>
2022-05-06 12:23:36 +1000

    updated HRIDs to be local record only
    Signed-off-by: Brian Ballsun-Stanton <Denubis@users.noreply.github.com>

---
+++ loadGroundwater.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export SERVER_FULLNAME="${1:-demo.db.faims.edu.au}" # dev, alpha, testing, selenium
+cp groundwater.json faims3-temp-notebook-migrator/testnotebook.json
+cp groundwater.json faims3-temp-notebook-migrator/
+cd faims3-temp-notebook-migrator
+echo "Load Groundwater ${SERVER_FULLNAME}"
+bash loadNotebook.sh $SERVER_FULLNAME "Macquarie Groundwater Notebook" "groundwater.json"
